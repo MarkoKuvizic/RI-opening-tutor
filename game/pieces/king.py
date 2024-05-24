625b3e66741d91b6bea9c8cd3a0ca1b1cd779683
milicamisic <milicamisic2303@gmail.com>
2024-05-24 15:52:31 +0200

    pawn available moves done

--- game/pieces/king.py
+++ game/pieces/king.py
@@ -4,15 +4,28 @@ class King(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         self.pgn_code = 'K'
-        self.available_moves = [[-1,-1], [-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
+        row = self.position[0]
+        col = self.position[1]
+        self.available_moves = [[row-1,col-1], [row-1,col],[row-1,col+1],[row,col-1],[row,col+1],[row+1,col-1],[row+1,col],[row+1,col+1]]
         
-    def get_legal_moves(self, board):
-        for move in self.available_moves:
-            future_position = self.position[0] + move[0]
+    def get_legal_moves(self, board: Board):
+        legal_moves = super.get_legal_moves()
+
+        return [move for move in legal_moves if not self.move_checks(move, board)]
+
             
     def is_in_check_from(self, piece: Piece, board: Board):
         if self.position in piece.get_check_moves(board):
             return True
+        
     def move_checks_from(self, move, board: Board, piece:Piece):
         if move in piece.get_check_moves(board):
             return True
+    def move_checks(self, move, board: Board):
+        for row in board.fields:
+            for pos in row:
+                if pos and pos.color != self.color:
+                    if self.move_checks_from(move, board, pos):
+                        return True
+        return False
--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -4,3 +4,30 @@ class Pawn(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         self.pgn_code = ''  # Pawns do not have a letter in PGN notation
+        self.direction = -1 if color == 'white' else 1
+
+    def get_legal_moves(self, board):
+        legal_moves = []
+        row, col = self.position
+        
+        # Standard one square forward move
+        forward_row = row + self.direction
+        if 0 <= forward_row < 8 and board[forward_row, col] is None:
+            legal_moves.append([forward_row, 0])
+            
+            # Two squares forward move if on the starting row
+            starting_row = 6 if self.color == 'white' else 1
+            if row == starting_row:
+                two_squares_forward_row = row + 2 * self.direction
+                if 0 <= two_squares_forward_row < 8 and board[two_squares_forward_row, col] is None:
+                    legal_moves.append([two_squares_forward_row, 0])
+
+        # Capture moves (diagonal forward)
+        for dc in [-1, 1]:
+            capture_row = row + self.direction
+            capture_col = col + dc
+            if 0 <= capture_row < 8 and 0 <= capture_col < 8:
+                if board[capture_row, capture_col] is not None and board[capture_row, capture_col].color != self.color:
+                    legal_moves.append([capture_row, capture_col])
+
+        return legal_moves
