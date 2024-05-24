b5710e111094963a541ace4ab42265a89ef186a2
milicamisic <milicamisic2303@gmail.com>
2024-05-24 14:35:15 +0200

    cao marko

--- game/pieces/king.py
+++ game/pieces/king.py
@@ -3,3 +3,9 @@ class King(Piece):
     def __init__(self, color):
         super().__init__(color)
         self.pgn_code = 'K'
+        self.available_moves = [[-1,-1], [-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+
+    def get_legal_moves(self, board):
+        for move in self.available_moves:
+            future_position = self.position[0] + move[0]
+        
--- game/pieces/piece.py
+++ game/pieces/piece.py
@@ -2,6 +2,7 @@ class Piece():
     def __init__(self, color):
         self.color = color
         self.pgn_code = ''
+        self.position = []
 
     def __str__(self):
         return f"{self.color} {self.__class__.__name__} ({self.pgn_code})"
