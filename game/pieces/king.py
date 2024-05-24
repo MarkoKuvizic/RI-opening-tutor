99eb7915539a5cac3af5323861759f3b0331a162
Marko Kuvizic <Marko.Kuvizic@gmail.com>
2024-05-24 15:23:39 +0200

    [Add] Added check check

--- game/pieces/king.py
+++ game/pieces/king.py
@@ -1,11 +1,18 @@
 from pieces.piece import Piece
+from game.board import Board
 class King(Piece):
     def __init__(self, color, position):
         super().__init__(color, position)
         self.pgn_code = 'K'
         self.available_moves = [[-1,-1], [-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-
+        
     def get_legal_moves(self, board):
         for move in self.available_moves:
             future_position = self.position[0] + move[0]
-        
+            
+    def is_in_check_from(self, piece: Piece, board: Board):
+        if self.position in piece.get_check_moves(board):
+            return True
+    def move_checks_from(self, move, board: Board, piece:Piece):
+        if move in piece.get_check_moves(board):
+            return True
--- game/pieces/piece.py
+++ game/pieces/piece.py
@@ -3,7 +3,10 @@ class Piece():
         self.color = color
         self.pgn_code = ''
         self.position = position
-
+    def get_legal_moves(self, board):
+        return []
+    def get_check_moves(self, board):
+        return []
     def __str__(self):
         return f"{self.color} {self.__class__.__name__} ({self.pgn_code})"
     
