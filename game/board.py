cd1e5aa42b32d8b4a13aeb8d0117f1bc31564e17
milicamisic <milicamisic2303@gmail.com>
2024-06-26 19:10:42 +0200

    [FIX] piece order, legal moves queen rook bishop, piece wont leave king in check

--- game/board.py
+++ game/board.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from pieces import rook, king, pawn, knight, bishop, queen
+
 class Board():
 
     def __init__(self):
@@ -22,33 +25,45 @@ class Board():
         self.fields[7][5] = bishop.Bishop("white", [7, 5])
         
         self.fields[0][3] = queen.Queen("black", [0, 3])
-        self.fields[7][4] = queen.Queen("white", [7, 4])
+        self.fields[7][3] = queen.Queen("white", [7, 3])
         
         self.fields[0][4] = king.King("black", [0, 4])
-        self.fields[7][3] = king.King("white", [7, 3])
+        self.fields[7][4] = king.King("white", [7, 4])
         
         self.fields[1] = [pawn.Pawn("black", [1, j]) for j in range(8)]
         self.fields[6] = [pawn.Pawn("white", [6, j]) for j in range(8)]
-
-    def print_board(self):
-        for row in self.fields:
-            print(row)
             
     def execute_move(self, position, move):
-        self.reset_en_passant()
+        print("MILICA")
         piece = self.fields[position[0]][position[1]]
-        # We need to check whether it's en-passant before we change the position
-        en_passant = isinstance(piece, pawn.Pawn) and piece.is_en_passant_move(self.fields, move)
+
+        # Execute the move
         piece.position = move
         self.fields[position[0]][position[1]] = None
+        destination_before_move = self.fields[move[0]][move[1]]
         self.fields[move[0]][move[1]] = piece
+
+        # Check if the move puts the current player's king in check
+        if self.is_king_in_check():
+            # Revert the move
+            piece.position = position
+            self.fields[position[0]][position[1]] = piece
+            self.fields[move[0]][move[1]] = destination_before_move
+            return # Move would leave the king in check, so it's not allowed
+        
+        self.reset_en_passant()
+        # We need to check whether it's en-passant before we change the position
+        en_passant = isinstance(piece, pawn.Pawn) and piece.is_en_passant_move(self.fields, move)
+
         if (en_passant):
             self.execute_en_passant(move)
+
         if isinstance(piece, pawn.Pawn):
             if abs(move[0] - position[0]) > 1:
                 piece.en_passantable = True
             else:
                 piece.en_passantable = False
+
         self.change_player()
 
     def execute_en_passant(self, move):
@@ -61,6 +76,53 @@ class Board():
                 if isinstance(piece, pawn.Pawn):
                     piece.en_passantable = False
 
+    def is_king_in_check(self):
+        # Find the position of the king of the specified color
+        king_position = None
+        for row in range(8):
+            for col in range(8):
+                piece = self.fields[row][col]
+                if isinstance(piece, king.King) and piece.color == self.player:
+                    king_position = [row, col]
+                    break
+            if king_position:
+                break
+        
+        if not king_position:
+            return False  # King of the specified color not found (should not happen in a valid game)
+
+        # Check if any opponent's pieces have legal moves that attack the king
+        opponent_color = "white" if self.player == "black" else "black"
+        for row in range(8):
+            for col in range(8):
+                piece = self.fields[row][col]
+                if piece and piece.color == opponent_color:
+                    legal_moves = piece.get_legal_moves(self.fields)
+                    if king_position in legal_moves:
+                        return True
+        
+        return False
+    
+    def is_checkmate(self, color):
+        # Check if the king of the specified color has any legal moves or can be protected by other pieces
+        for row in range(8):
+            for col in range(8):
+                piece = self.fields[row][col]
+                if piece and piece.color == color:
+                    legal_moves = piece.get_legal_moves(self.fields)
+                    for move in legal_moves:
+                        # Simulate the move to see if it resolves the check
+                        current_position = piece.position
+                        self.execute_move(current_position, move)
+                        if not self.is_king_in_check(color):
+                            # Undo the move
+                            self.execute_move(move, current_position)
+                            return False
+                        # Undo the move
+                        self.execute_move(move, current_position)
+        
+        return True
+    
     def change_player(self):
         if self.player == "white":
             self.player = "black"
--- game/pieces/bishop.py
+++ game/pieces/bishop.py
@@ -1,15 +1,18 @@
 from pieces.piece import Piece
 class Bishop(Piece):
+
     def __init__(self, color, position):
         super().__init__(color, position)
         self.pgn_code = 'B'
         self.img = color[0] + "b"
         self.available_moves = []
+
     def get_legal_moves(self, board):
         row = self.position[0]
         col = self.position[1]
         axes = []
         board_size = 8
+
         axis1 = [[row + i, col + i] for i in range(1, board_size) if 0 <= row + i < board_size and 0 <= col + i < board_size]
         axes.append(sorted(axis1, key = lambda x: abs(x[0] - row) + abs(x[1] - col)))
         
@@ -26,11 +29,13 @@ class Bishop(Piece):
         for axis in axes:
             for move in axis:
                 if self.empty_or_can_eat(move[0], move[1], board):
-                    print(move)
+                    #print(move)
                     legal_moves.append(move)
+                    if board[move[0]][move[1]] is not None:
+                        break  # Stop further moves in this direction if a piece is encountered
                 else:
                     break
         return legal_moves
         
-b = Bishop("white", [3,3])
-print(b.available_moves)
+# b = Bishop("white", [3,3])
+# print(b.available_moves)
