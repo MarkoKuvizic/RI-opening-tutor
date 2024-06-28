f58cd42a6e07570ac6366120c5e203a0b599fe86
milicamisic <milicamisic2303@gmail.com>
2024-06-28 13:19:23 +0200

    [ADD] PGN play done, without promotion

--- game/board.py
+++ game/board.py
@@ -165,3 +165,87 @@ class Board():
             self.player = "black"
             return
         self.player = "white"
+
+    
+    def get_piece_and_move(self, move):
+        col_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        row_dict = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+        piece = None
+        move_cords = None
+
+        if move[-1] in '+#':
+            move_cords = [row_dict[move[-2]], col_dict[move[-3]]]
+            move = move[0:-1]
+
+        elif 'O' not in move:
+            move_cords = [row_dict[move[-1]], col_dict[move[-2]]]
+
+        if move[0] not in 'NBRQKO':
+            piece = self.get_pawn(move, move_cords)
+
+        elif move[0] in 'KO': # za rokade isto kralj
+            piece = self.get_king()
+            if move == 'O-O': move_cords = [piece.position[0], piece.position[1] + 2]
+            elif move == 'O-O-O': move_cords = [piece.position[0], piece.position[1] - 2]
+
+        else:
+            piece = self.get_piece(move, move_cords)
+
+        return piece, move_cords
+                
+    def get_pawn(self, move, move_cords):
+        if 'x' not in move:
+            for row in range(8):
+                for col in range(8):
+                    piece = self.fields[row][col]
+                    if piece and piece.color == self.player and isinstance(piece, pawn.Pawn) and move_cords in piece.get_legal_moves(self.fields):
+                        return piece
+        else:
+            col_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+            col = col_dict[move[0]]
+            for row in range(8):
+                piece = self.fields[row][col]
+                if piece and piece.color == self.player and isinstance(piece, pawn.Pawn) and move_cords in piece.get_legal_moves(self.fields):
+                    return piece
+            
+    def get_piece(self, move, move_cords):
+        col_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+        row_dict = {'1': 7, '2': 6, '3': 5, '4': 4, '5': 3, '6': 2, '7': 1, '8': 0}
+        piece_dict = {'N': knight.Knight, 'B': bishop.Bishop, 'R': rook.Rook, 'Q': queen.Queen}
+
+        def is_valid_piece(piece):
+            return piece and piece.color == self.player and isinstance(piece, piece_dict[move[0]]) and move_cords in piece.get_legal_moves(self.fields)
+
+        def find_piece(move_cords, col=None, row=None):
+            for r in range(8):
+                for c in range(8):
+                    if col is not None and c != col:
+                        continue
+                    if row is not None and r != row:
+                        continue
+                    piece = self.fields[r][c]
+                    if is_valid_piece(piece):
+                        return piece
+            return None
+
+        if len(move) == 3 or (len(move) == 4 and 'x' in move):
+            return find_piece(move_cords)
+
+        elif move[1] in col_dict.keys():
+
+            if move[2] in row_dict.keys(): # ako imamo bas tacnu pocetnu lokaciju
+                return find_piece(move_cords, col=col_dict[move[1]], row=row_dict[move[1]])
+
+            return find_piece(move_cords, col=col_dict[move[1]])
+                
+        elif move[1] in row_dict.keys():
+            return find_piece(move_cords, row=row_dict[move[1]])
+    
+    def get_king(self):
+        for row in range(8):
+            for col in range(8):
+                piece = self.fields[row][col]
+                if piece and piece.color == self.player and isinstance(piece, king.King):
+                    return piece
+    
+        
--- game/main.py
+++ game/main.py
@@ -46,6 +46,15 @@ def get_square_under_mouse():
     col = x // SQUARE_SIZE
     return row, col
 
+def parse_pgn(pgn):
+    moves = []
+    for line in pgn.split('\n'):
+        if not line.strip().startswith('['):
+            moves.extend(line.split(' '))
+
+    moves = [move for move in moves if move and not move[0].isdigit() and not move.endswith('.')]
+    return moves
+
 def main():
     board = Board()
     board.setup()
@@ -59,44 +68,102 @@ def main():
     run = True
     checkmate = False
 
+    # PGN string (example)
+    pgn = """
+            1. e4 d5 2. Qf3 dxe4 3. Qxe4 Nf6 4. Qe5 e6 5. Bb5+ Bd7 6. Bxd7+ Qxd7 7. c4 Nc6 8. Qb5 O-O-O 9. d4 a6 10. Qb3 Nxd4 11. Qc3 Ne4 12. Qe3 Nc2+ 13. Ke2 Qd1# 0-1
+
+            [Event "Rated Blitz game"]
+            [Site "https://lichess.org/pjIgfKFJ"]
+            [White "BlackBranca"]
+            [Black "HardwellOfKing"]
+            [Result "1-0"]
+            [UTCDate "2015.12.31"]
+            [UTCTime "23:01:53"]
+            [WhiteElo "1817"]
+            [BlackElo "1445"]
+            [WhiteRatingDiff "+9"]
+            [BlackRatingDiff "-11"]
+            [ECO "B32"]
+            [Opening "Sicilian Defense: Franco-Sicilian Variation"]
+            [TimeControl "180+2"]
+            [Termination "Normal"]
+            """
+    
+    pgn2 = """
+            1. e4 c5 2. g3 Nc6 3. Bg2 d6 4. c3 g6 5. d3 Bg7 6. Be3 Nf6 7. Qd2 Ng4 8. Bf4 h6 9. h3 Nf6 10. Nf3 g5 11. Be3 Be6 12. Na3 a6 13. Rb1 Qa5 14. c4 Qxd2+ 15. Bxd2 Nd7 16. O-O Nde5 17. Ne1 Nb4 18. Bxb4 cxb4 19. Nac2 Nxc4 20. dxc4 Bxc4 21. e5 Bxe5 22. Bxb7 Rb8 23. Bc6+ Kd8 24. f4 gxf4 25. gxf4 Rg8+ 26. Kh1 Bxf1 27. fxe5 dxe5 28. Kh2 Kc7 29. Be4 Rgd8 30. Ne3 Bb5 31. Nf3 Bd3 32. Rc1+ Kb6 33. Nd5+ Rxd5 34. Bxd5 e6 35. Nxe5 Rd8 36. Bg2 Bb5 37. Nxf7 Rd2 38. Nxh6 Rxb2 39. Ra1 Bc6 40. Rg1 Rxa2 41. Kg3 Rxg2+ 42. Rxg2 Bxg2 43. Kxg2 b3 44. Ng4 b2 45. Ne3 b1=Q 46. h4 Qe4+ 47. Kf2 Qxh4+ 48. Ke2 Qe4 49. Kd2 Kc5 50. Nc2 a5 51. Ne1 Kc4 52. Kc1 Qxe1+ 0-1
+
+            [Event "Rated Blitz game"]
+            [Site "https://lichess.org/XuoITSbx"]
+            [White "Taiane"]
+            [Black "javoaussin"]
+            [Result "0-1"]
+            [UTCDate "2015.12.31"]
+            [UTCTime "23:01:49"]
+            [WhiteElo "1899"]
+            [BlackElo "1788"]
+            [WhiteRatingDiff "-17"]
+            [BlackRatingDiff "+22"]
+            [ECO "B09"]
+            [Opening "Pirc Defense: Austrian Attack #2"]
+            [TimeControl "180+2"]
+            [Termination "Time forfeit"]
+            """
+
+    moves = parse_pgn(pgn2)
+    move_delay = 500  # milliseconds between moves
+
+    # print(moves)
+    # s = input()
+
     while run:
         clock.tick(60)  # Limit the frame rate to 60 frames per second
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN and not checkmate:
-                row, col = get_square_under_mouse()
-                if selected_piece:
-                    # Move the piece to the new location
-                    if [row, col] in possible_moves:
-                        checkmate = board.execute_move(selected_piece, [row, col])
-                        selected_piece = None
-                    else:
-                        if board.fields[row][col] and board.fields[row][col].color == board.player:
-                            selected_piece = [row, col]
-                else:
-                    # Select the piece
-                    if board.fields[row][col] and board.fields[row][col].color == board.player:
-                        selected_piece = [row, col]
-                    
+
+        for move in moves:
+            draw_board(WIN)
+            draw_pieces(WIN, board)
+            pygame.display.flip()  # Update the display
+            selected_piece, move_coords = board.get_piece_and_move(move)
+            possible_moves = selected_piece.get_legal_moves(board.fields)
+            if move_coords in possible_moves:
+                checkmate = board.execute_move(selected_piece.position, move_coords)
+
+            pygame.time.wait(move_delay)
+
+            # if event.type == pygame.MOUSEBUTTONDOWN and not checkmate:
+            #     row, col = get_square_under_mouse()
+            #     if selected_piece:
+            #         # Move the piece to the new location
+            #         if [row, col] in possible_moves:
+            #             checkmate = board.execute_move(selected_piece, [row, col])
+            #             selected_piece = None
+            #         else:
+            #             if board.fields[row][col] and board.fields[row][col].color == board.player:
+            #                 selected_piece = [row, col]
+            #     else:
+            #         # Select the piece
+            #         if board.fields[row][col] and board.fields[row][col].color == board.player:
+            #             selected_piece = [row, col]
+
+        # if selected_piece:
+        #     # Highlight the selected piece
+        #     row, col = selected_piece
+        #     #pygame.draw.rect(WIN, HIGHLIGHT, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 3)
+        #     piece = board.fields[selected_piece[0]][selected_piece[1]]
+        #     possible_moves = []
+        #     if piece:
+        #         possible_moves = piece.get_legal_moves(board.fields)
+            
+        #     for move in possible_moves:
+        #         [r, c] = move
+        #         pygame.draw.rect(WIN, HIGHLIGHT, (c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 3)
+
         draw_board(WIN)
         draw_pieces(WIN, board)
-
-        if selected_piece:
-            # Highlight the selected piece
-            row, col = selected_piece
-            #pygame.draw.rect(WIN, HIGHLIGHT, (col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 3)
-            piece = board.fields[selected_piece[0]][selected_piece[1]]
-            possible_moves = []
-            if piece:
-                possible_moves = piece.get_legal_moves(board.fields)
-            
-            for move in possible_moves:
-                [r, c] = move
-                pygame.draw.rect(WIN, HIGHLIGHT, (c * SQUARE_SIZE, r * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 3)
-
-
         pygame.display.flip()  # Update the display
+        pygame.time.wait(5000)
 
         if checkmate:
             print("Checkmate! Game over.")
